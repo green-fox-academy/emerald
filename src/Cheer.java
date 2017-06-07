44eac53f8fcab3ff8ee0bd9cc2e5fa573ebfde89
Zsuzsanna Padar <zsuzsanna.padar@gmail.com>
2017-06-07 15:05:48 +0200

    b5-1

--- src/Cheer.java
+++ src/Cheer.java
@@ -1,6 +1,7 @@
 public class Cheer {
-  public static void cheer(){
-    System.out.println("Java");
+
+  public static void cheer(String parameterOne, String parameterTwo){
+    System.out.println(parameterOne + parameterTwo);
   }
 
 }
