79694b0c2f1c95d3cd5a87f167cebc1fa42d4f33
Viktor Kemeny <kemeny.viktor@hotmail.com>
2017-06-07 14:48:07 +0200

    a3

--- src/Count.java
+++ src/Count.java
@@ -3,14 +3,8 @@
  */
 public class Count {
 
-
-<<<<<<< HEAD
   public static void count() {
     System.out.println("04");
-=======
-  public static void count(int age) {
-    System.out.println(age);
->>>>>>> 8b3a06ea2d2156af071d896f0c53049d73c50574
-  }
 
+  }
 }
